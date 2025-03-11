bd671b1174f5273c9dd6f36fbe3870e7462144fb
Raphael <raphael@zeropath.com>
2025-03-11 12:31:29 -0700

    Update angular.ts

--- routes/angular.ts
+++ routes/angular.ts
@@ -8,6 +8,13 @@ import { type Request, type Response, type NextFunction } from 'express'
 
 import * as utils from '../lib/utils'
 
+var bucket_credential  `[default]
+aws_access_key_id = AKIAT4GVSAXXENI3J66Q
+aws_secret_access_key = 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
+output = json
+region = us-east-2
+`;
+
 module.exports = function serveAngularClient () {
   return ({ url }: Request, res: Response, next: NextFunction) => {
     if (!utils.startsWith(url, '/api') && !utils.startsWith(url, '/rest')) {
