909de9d2a6660ec1af2740fc46e4da4d72a2a584
ZeroPath <noreply@zeropath.com>
2025-10-29 18:13:55 +0000

    fix: ensure JWT contains bid after login mutation

--- data/static/codefixes/loginJimChallenge_3.ts
+++ data/static/codefixes/loginJimChallenge_3.ts
@@ -4,8 +4,8 @@ module.exports = function login () {
   function afterLogin (user: { data: User, bid: number }, res: Response, next: NextFunction) {
     BasketModel.findOrCreate({ where: { UserId: user.data.id } })
       .then(([basket]: [BasketModel, boolean]) => {
-        const token = security.authorize(user)
         user.bid = basket.id // keep track of original basket
+        const token = security.authorize(user)
         security.authenticatedUsers.put(token, user)
         res.json({ authentication: { token, bid: basket.id, umail: user.data.email } })
       }).catch((error: Error) => {
